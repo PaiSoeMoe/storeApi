54d057f4059f9cfce1add2eec52bf694f5172608
PaiSoeMoe <paisoe1991@gmail.com>
2020-05-04 22:01:48 -0400

    debug

--- men.js
+++ men.js
@@ -3,18 +3,19 @@ const router = express.Router();
 const product = require('./models/men');
 
 
-router.get('/', async (req, res) => {
-    let cate = req.query.subcategory;
-    if (req.query.id !== "undefined") {
-        res.send(await product.findById(id));
-    }
-    else if (cate === 'all') {
+router.get('/', (req, res) => {
+    res.send("men");
+    // let cate = req.query.subcategory;
+    // if (req.query.id !== "undefined") {
+    //     res.send(await product.findById(id));
+    // }
+    // else if (cate === 'all') {
 
-        res.send(await product.find({}));
-    } else {
-        res.send(await product.find({ category: cate }))
+    //     res.send(await product.find({}));
+    // } else {
+    //     res.send(await product.find({ category: cate }))
 
-    }
+    // }
 })
 
 
