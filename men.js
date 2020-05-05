33376469b1679c4f143c200a5b48800c5279803c
PaiSoeMoe <paisoe1991@gmail.com>
2020-05-05 11:57:51 -0400

    fixed JSON problem

--- men.js
+++ men.js
@@ -8,13 +8,13 @@ router.get('/', async (req, res) => {
     let cate = req.query.subcategory;
 
     if (req.query.id !== "undefined") {
-        res.send(await product.findById(id));
+        res.send(JSON.parse(await product.findById(id)));
     }
     else if (cate === 'all') {
 
-        res.send(await product.find({}));
+        res.send(JSON.parse(await product.find({})));
     } else {
-        res.send(await product.find({ category: cate }))
+        res.send(JSON.parse(await product.find({ category: cate })))
 
     }
 })
