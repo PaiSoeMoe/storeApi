241fd6e687246ae1b54b11aa54e81d0007f058b5
PaiSoeMoe <paisoe1991@gmail.com>
2020-05-04 23:38:21 -0400

    mongo setup

--- men.js
+++ men.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const product = require('./models/men');
 
 
-mongoose.model('haha', haha);
 router.get('/', async (req, res) => {
 
     //let cate = req.query.subcategory;
